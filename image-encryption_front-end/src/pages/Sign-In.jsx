458e955124d03bb3de0e241acaffd42f028eeb79
sb2bg <sullivanbognar@gmail.com>
2022-09-05 02:25:44 -0500

    Login/register functionality

--- image-encryption_front-end/src/pages/Sign-In.jsx
+++ image-encryption_front-end/src/pages/Sign-In.jsx
@@ -1,40 +1,50 @@
+import { loginUser } from "../api";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 export default function SignIn() {
+  const navigate = useNavigate();
+  const [render, setRender] = useState(false);
+
   const login = (e) => {
     e.preventDefault();
 
-    fetch("http://localhost:8393/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: e.target.username.value,
-        password: e.target.password.value,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
+    loginUser(e.target.username.value, e.target.password.value).then((r) => {
+      if (r.data.error) {
+        alert(r.data.error);
+      } else if (r.data.success) {
+        navigate("/user");
+      }
+    });
   };
 
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/user");
+    } else {
+      setRender(true);
+    }
+  }, [navigate]);
+
   return (
-    <div>
-      <h1>Sign In</h1>
-      <form onSubmit={login}>
-        <div className="form-control">
-          <label htmlFor="username">Username</label>
-          <input type="username" id="username" />
+    render && (
+      <div>
+        <h1>Sign In</h1>
+        <form onSubmit={login}>
+          <div className="form-control">
+            <label htmlFor="username">Username</label>
+            <input type="username" id="username" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+        <div className="sign-up">
+          Need an account? <a href="/register">Sign Up</a>
         </div>
-        <div className="form-control">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-        </div>
-        <button type="submit">Sign In</button>
-      </form>
-      <div className="sign-up">
-        Need an account? <a href="/register">Sign Up</a>
       </div>
-    </div>
+    )
   );
 }
