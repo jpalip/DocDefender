4e60bf3c848822368d7442e1337c57eb67724cb1
sb2bg <sullivanbognar@gmail.com>
2022-09-05 20:43:49 -0500

    Move api to a hook and add redirect helpers

---
+++ image-encryption_front-end/src/hooks/hooks.js
@@ -0,0 +1,86 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export const useAuth = () => {
+  const navigate = useNavigate();
+
+  function authHeader() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      return { "x-access-token": token };
+    } else {
+      return {};
+    }
+  }
+
+  function logoutUser() {
+    localStorage.removeItem("token");
+    navigate("/sign-in");
+  }
+
+  function loginRegister(username, password, type) {
+    return axios
+      .post(`http://localhost:8393/${type}`, { username, password })
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem("token", response.data.token);
+        }
+
+        return response;
+      });
+  }
+
+  function loginUser(username, password) {
+    return loginRegister(username, password, "login");
+  }
+
+  function registerUser(username, password) {
+    return loginRegister(username, password, "register");
+  }
+
+  function handleErrorResponse(error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      logoutUser();
+    }
+
+    return error;
+  }
+
+  function getImages() {
+    return axios
+      .get("http://localhost:8393/images", {
+        headers: authHeader(),
+      })
+      .catch(handleErrorResponse)
+      .then((response) => {
+        return response;
+      });
+  }
+
+  function authed() {
+    return localStorage.getItem("token") !== null;
+  }
+
+  function redirectIfAuthed(href) {
+    if (authed()) {
+      navigate(href);
+    }
+  }
+
+  function redirectIfNotAuthed(href) {
+    if (!authed()) {
+      navigate(href);
+    }
+  }
+
+  return {
+    authed,
+    redirectIfAuthed,
+    redirectIfNotAuthed,
+    loginUser,
+    registerUser,
+    logoutUser,
+    getImages,
+  };
+};
