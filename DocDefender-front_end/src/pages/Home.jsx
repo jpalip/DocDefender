ccb52159fd194ff9accd29acbb08842c20bfca15
Night <jpaliprantis@gmail.com>
2022-10-05 16:31:50 -0400

    Removed node_modules + fixed html errors

--- DocDefender-front_end/src/pages/Home.jsx
+++ DocDefender-front_end/src/pages/Home.jsx
@@ -1,22 +1,12 @@
 export default function Home() {
   return (
-  <html>
-    <head>
-      <link rel="stylesheet" type="text/css" href="styles.css" />
-    </head>
-  
-  <body>
     <div id="page-container">
       <div id="content-wrap">
-        <div class ="fade-in-text">
-        <h3>Welcome to DocDefender.org!</h3>
+        <div className="fade-in-text">
+          <h3>Welcome to DocDefender.org</h3>
         </div>
       </div>
-      <footer id="footer">
-        &#169;DocDefender 2022
-      </footer>
+      <footer id="footer">&#169;DocDefender 2022</footer>
     </div>
-  </body>
-  </html>
   );
 }
