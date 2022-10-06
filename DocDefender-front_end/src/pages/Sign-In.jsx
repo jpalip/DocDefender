2f83719dd99f2a8beef0e54df29b35b04e6ebee2
Connor McGarvey <60416133+Connor-McGarvey@users.noreply.github.com>
2022-10-06 19:30:37 -0400

    add bootstrap to sign in (split page with logo)

--- DocDefender-front_end/src/pages/Sign-In.jsx
+++ DocDefender-front_end/src/pages/Sign-In.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/hooks";
+import logo from "./icon.png";
 
 export default function SignIn() {
   const navigate = useNavigate();
@@ -19,25 +20,58 @@ export default function SignIn() {
     });
   };
 
-  return (
+   return (
     !authed() && (
-      <div>
-        <h1>Sign In</h1>
-        <form onSubmit={login}>
-          <div className="form-control">
-            <label htmlFor="username">Username</label>
-            <input type="username" id="username" autoComplete="ie-username" />
+      <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="styles.css" />
+        </head>
+        <body>
+          <div class="container-fluid">
+            <div class="row no-gutter">
+              {/* image half */}
+              <div class="col-md-6 d-none d-md-flex bg-image">
+                <img src={logo} class="img-fluid" alt="responsive image is not avalible"></img>
+              </div>
+
+              {/* login form half */}
+              <div class="col-md-6 bg-light">
+                <div class="login d-flex align-items-center py-5">
+
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-lg-10 col-xl-7 mx-auto">
+                        <h3 class="display-4">Login</h3>
+                        <p class="text-muted mb-4">Login to DocDefender now or register an account below.</p>
+                        <form onSubmit={login}>
+                          {/* email input */}
+                          <div className="form-floating mb-3">
+                            <input type="username" id="username" class="form-control" />
+                            <label class="form-label" for="username">Username</label>
+                          </div>
+                          {/* password input */}
+                          <div className="form-floating mb-3">
+                            <input type="password" id="password" class="form-control" />
+                            <label class="form-label" for="password">Password</label>
+                          </div>
+                          <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                          {/* <button type="submit">Sign In</button> */}
+                          {/* need account ? */}
+                          <div class="text-center">
+                            <p> Not a member? <a href="/register">Register</a></p>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
           </div>
-          <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" autoComplete="ie-password" />
-          </div>
-          <button type="submit">Sign In</button>
-        </form>
-        <div className="sign-up">
-          Need an account? <a href="/register">Sign Up</a>
-        </div>
-      </div>
+        </body>
+      </html>
     )
   );
 }
