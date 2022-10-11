a3a9daa52263c74754360d39f798cfea4239fd2e
Night <jpaliprantis@gmail.com>
2022-10-11 18:34:05 -0400

    Updated html errors + typos

--- DocDefender-front_end/src/pages/About.jsx
+++ DocDefender-front_end/src/pages/About.jsx
@@ -1,101 +1,144 @@
 export default function About() {
   return (
-  <html>
-    <head>
-      <link rel="stylesheet" type="text/css" href="styles.css" />
-    </head>
-
-  <body>
     <div id="page-container">
       <div id="content-wrap">
-        <h2 style={{'font-family':''}}><u>Frequently Asked Questions</u></h2>
-        <br/>
-        <br/>
-        <div class="row">
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-white bg-dark mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">What is DocDefender?</div>
-                <div class="card-body">
-                <p class="card-text">DocDefender is a fast and reliable encrypyion service that uses RSA
-                and AES methods to provide a high degree of efficacy and reliability.</p>
-                </div>
-              </div>
-            </div>
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-light bg-secondary mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">Does DocDefender cost anything?</div>
-                <div class="card-body">
-                <p class="card-text">No! DocDefender is a free to use file encryption system.
-                No subscriptions or fees are required in order to register or maintain an account
-                with us.
+        <h2>
+          <u>Frequently Asked Questions</u>
+        </h2>
+        <br />
+        <br />
+        <div className="row">
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-white bg-dark mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">What is DocDefender?</div>
+              <div className="card-body">
+                <p className="card-text">
+                  DocDefender is a fast and reliable encryption service that
+                  uses RSA and AES methods to provide a high degree of efficacy
+                  and reliability.
                 </p>
               </div>
             </div>
           </div>
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-light bg-dark mb-3" style={{'max-width': '50rem'}}>
-              <div class="card-header fs-5">Can I encrypt any file?</div>
-                <div class="card-body">
-                <p class="card-text">Currently, DocDefender supports the encryption of documents and images.
-                Supported image formats include .pdf, .jpeg, .jpg, .png and .mov.</p>
-                </div>
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-light bg-secondary mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">
+                Does DocDefender cost anything?
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  No! DocDefender is a free to use file encryption system. No
+                  subscriptions or fees are required in order to register or
+                  maintain an account with us.
+                </p>
               </div>
             </div>
           </div>
-        <br/>
-        <div class="row">
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-light bg-secondary mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">Will my files be saved/stored when I upload them?</div>
-                <div class="card-body">
-                <p class="card-text">We value the privacy of our users. As such, all files uploaded to
-                DocDefender are encrypted but not saved.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-light bg-dark mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">Am I the only one able to see my encrypted files?</div>
-                <div class="card-body">
-                <p class="card-text">Yes! Once a file is encrypted, only the user and others that the user
-                has given permissions to may view the contents of the file. DocDefender
-                staff will not be able to see the contents of encrypted files.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-light bg-secondary mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">I need help with my account!</div>
-                <div class="card-body">
-                <p class="card-text">We're always willing to help! For any questions about
-                registration, account settings or terminating an account, send us an email
-                using the link below. Users will typically receive a response within a few hours.</p>
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-light bg-dark mb-3"
+              style={{ maxWidth: "50rem" }}
+            >
+              <div className="card-header fs-5">Can I encrypt any file?</div>
+              <div className="card-body">
+                <p className="card-text">
+                  Currently, DocDefender supports the encryption of documents
+                  and images. Supported image formats include .pdf, .jpeg, .jpg,
+                  .png and .mov.
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <br/>
-        <div class = "row justify-content-center">
-          <div class="col-md-4 cssanimation sequence fadeInBottom">
-            <div class="card border-info text-dark bg-warning mb-3" style={{'max-width': '25rem'}}>
-              <div class="card-header fs-5">
-                <a class="btn btn-primary" href="mailto:docdefender@gmail.com">Contact Us!</a>
-                </div>
-                <div class="card-body">
-                <p class="card-text">We'd love to hear from you! For any inquiries or suggestions 
-                please click the link above. For support questions, please
-                make sure to have your account information ready as we'll need to verify your
-                identity before we can proceed.</p>
-                </div>
+        <br />
+        <div className="row">
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-light bg-secondary mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">
+                Will my files be saved/stored when I upload them?
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  We value the privacy of our users. As such, all files uploaded
+                  to DocDefender are encrypted but not saved.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-light bg-dark mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">
+                Am I the only one able to see my encrypted files?
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  Yes! Once a file is encrypted, only the user and others that
+                  the user has given permissions to may view the contents of the
+                  file. DocDefender staff will not be able to see the contents
+                  of encrypted files.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-light bg-secondary mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">
+                I need help with my account!
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  We're always willing to help! For any questions about
+                  registration, account settings or terminating an account, send
+                  us an email using the link below. Users will typically receive
+                  a response within a few hours.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div className="row justify-content-center">
+          <div className="col-md-4 cssanimation sequence fadeInBottom">
+            <div
+              className="card border-info text-dark bg-warning mb-3"
+              style={{ maxWidth: "25rem" }}
+            >
+              <div className="card-header fs-5">
+                <a
+                  className="btn btn-primary"
+                  href="mailto:docdefender@gmail.com"
+                >
+                  Contact Us!
+                </a>
+              </div>
+              <div className="card-body">
+                <p className="card-text">
+                  We'd love to hear from you! For any inquiries or suggestions
+                  please click the link above. For support questions, please
+                  make sure to have your account information ready as we'll need
+                  to verify your identity before we can proceed.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-        <footer id="footer">
-          &#169;DocDefender 2022
-        </footer>
-      </div>
-  </body>
-  </html>
+      <footer id="footer">&#169;DocDefender 2022</footer>
+    </div>
   );
 }
--- DocDefender-front_end/src/pages/Sign-In.jsx
+++ DocDefender-front_end/src/pages/Sign-In.jsx
@@ -20,58 +20,77 @@ export default function SignIn() {
     });
   };
 
-   return (
+  return (
     !authed() && (
-      <html>
-        <head>
-          <link rel="stylesheet" type="text/css" href="styles.css" />
-        </head>
-        <body>
-          <div class="container-fluid">
-            <div class="row no-gutter">
-              {/* image half */}
-              <div class="col-md-6 d-none d-md-flex bg-image">
-                <img src={logo} class="img-fluid" alt="responsive image is not avalible"></img>
-              </div>
+      <div className="container-fluid">
+        <div className="row no-gutter">
+          {/* image half */}
+          {/* <div class="col-md-6 d-none d-md-flex bg-image">
+            <img
+              src={logo}
+              class="img-fluid"
+              alt="responsive image is not avalible"
+            ></img>
+          </div> */}
 
-              {/* login form half */}
-              <div class="col-md-6 bg-light">
-                <div class="login d-flex align-items-center py-5">
-
-                  <div class="container">
-                    <div class="row">
-                      <div class="col-lg-10 col-xl-7 mx-auto">
-                        <h3 class="display-4">Login</h3>
-                        <p class="text-muted mb-4">Login to DocDefender now or register an account below.</p>
-                        <form onSubmit={login}>
-                          {/* email input */}
-                          <div className="form-floating mb-3">
-                            <input type="username" id="username" class="form-control" />
-                            <label class="form-label" for="username">Username</label>
-                          </div>
-                          {/* password input */}
-                          <div className="form-floating mb-3">
-                            <input type="password" id="password" class="form-control" />
-                            <label class="form-label" for="password">Password</label>
-                          </div>
-                          <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                          {/* <button type="submit">Sign In</button> */}
-                          {/* need account ? */}
-                          <div class="text-center">
-                            <p> Not a member? <a href="/register">Register</a></p>
-                          </div>
-                        </form>
+          {/* login form half */}
+          <div className="center-screen">
+            <div className="login d-flex align-items-center py-5">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-10 col-xl-7 mx-auto">
+                    <h3 className="display-4">
+                      <b>Login</b>
+                    </h3>
+                    <p className="text-muted mb-4">
+                      Login to DocDefender now or register an account below.
+                    </p>
+                    <form onSubmit={login}>
+                      {/* email input */}
+                      <div className="form-floating mb-3">
+                        <input
+                          type="username"
+                          id="username"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="username">
+                          Username
+                        </label>
                       </div>
-                    </div>
+                      {/* password input */}
+                      <div className="form-floating mb-3">
+                        <input
+                          type="password"
+                          id="password"
+                          className="form-control"
+                        />
+                        <label className="form-label" htmlFor="password">
+                          Password
+                        </label>
+                      </div>
+                      <button
+                        type="submit"
+                        className="btn btn-primary btn-block"
+                      >
+                        Sign In
+                      </button>
+                      {/* <button type="submit">Sign In</button> */}
+                      {/* need account ? */}
+                      <div className="text-center">
+                        <br />
+                        <h5>
+                          {" "}
+                          Not a member? <a href="/register">Register</a>
+                        </h5>
+                      </div>
+                    </form>
                   </div>
-
                 </div>
               </div>
-
             </div>
           </div>
-        </body>
-      </html>
+        </div>
+      </div>
     )
   );
 }
