561496c0c06d495ecd996612c36f8236e24adc5b
Logan Weiner <lweiner@albany.edu>
2022-11-01 13:59:06 -0400

    Created Terms page

--- DocDefender-front_end/src/App.jsx
+++ DocDefender-front_end/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/Sign-In";
 import Register from "./pages/Register";
+import Terms from "./pages/Terms";
 import { Route, Routes } from "react-router-dom";
 import User from "./pages/User";
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/register" element={<Register />} />
           <Route path="/user" element={<User />} />
+          <Route path="/terms" element={<Terms />} />
         </Routes>
       </div>
     </>
