7eb286f9e32f9b9ac17a8e3801ba18719ad43d45
Night <jpaliprantis@gmail.com>
2022-11-07 23:59:28 -0500

    Fixed duplicate filenames, add (#) to name

--- DocDefender-back_end/routes/upload.js
+++ DocDefender-back_end/routes/upload.js
@@ -5,13 +5,41 @@ import multerS3 from "multer-s3";
 export default async (req, res) => {
   var filename;
 
+  const getFileType = (param) => param.split(".").pop();
+  const getFileName = (param) =>
+    param.substring(0, param.length - getFileType(param).length);
+
   const upload = multer({
     storage: multerS3({
       s3,
       bucket: process.env.DO_SPACES_NAME,
       acl: "",
-      key: function (request, file, cb) {
-        cb(null, file.originalname);
+      key: async function (request, file, cb) {
+        // The Following code prevents duplicate file names - It adds (1),(2),(3),...etc to the filename if it exists already, then uploads it
+        var fileExists = await prisma.file.count({
+          where: {
+            title: filename,
+          },
+        });
+
+        var title = getFileName(file.originalname);
+        var fileType = getFileType(file.originalname);
+
+        console.log(title, fileType);
+
+        var count = 1;
+        while (fileExists > 0) {
+          title += "(" + count.toString() + ")";
+          filename = title + "." + fileType;
+          count++;
+          fileExists = await prisma.file.count({
+            where: {
+              title: filename,
+            },
+          });
+        }
+
+        cb(null, filename); // file.originalname is the name uploaded to Bucket
       },
     }),
   }).single("file");
@@ -21,13 +49,15 @@ export default async (req, res) => {
       console.log(err);
       return res.json({ error: err.message });
     }
-    filename = req.file.originalname;
+
     var params = {
       Bucket: process.env.DO_SPACES_NAME,
       Key: filename,
       Expires: 60,
     };
+
     var fileURL = s3.getSignedUrl("getObject", params);
+
     await prisma.file.create({
       data: {
         authorId: req.id,
